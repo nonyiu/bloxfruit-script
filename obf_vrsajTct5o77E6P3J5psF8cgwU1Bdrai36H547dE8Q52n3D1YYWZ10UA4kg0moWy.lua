19637e6ae0fb26d7c952f6e9c9308ad41c9f6990
nonyiu <pearlelsine@gmail.com>
2024-12-28 02:12:27 +0800

    Add files via upload

---
+++ obf_vrsajTct5o77E6P3J5psF8cgwU1Bdrai36H547dE8Q52n3D1YYWZ10UA4kg0moWy.lua
@@ -0,0 +1,12 @@
+local v0=16164 + (((3186 + 678 + (1585442 -1221341)) -(274513 -(32 + 85))) -(66249 + 1351)) + ((42345 + 148607) -110638) ;v0=v0 + ((1070 -(892 + 65)) -((61 -35) + 67)) + (1219 -((219 -100) + (1830 -833))) ;local v1=1203456;local v2=1230471;local v3=8023481;if (v2>v1) then print("true");end if (((351 -(87 + 263)) + v3)>v2) then print("obfuscate the conditions!");end print("Clicking [Strings] will completely hide this string!");do function sieve_of_eratosthenes(v5) local v6=0;local v7;while true do local v10=180 -(67 + 113) ;while true do if (v10==(0 + 0)) then if ((0 -0)==v6) then local v11=0;while true do if (v11==1) then v6=1 + 0 ;break;end if ((0 -0)==v11) then v7={};for v13=1,v5 do v7[v13]=(953 -(802 + 150))~=v13 ;end v11=1;end end end if (v6==(2 -1)) then for v12=3 -1 ,math.floor(math.sqrt(v5)) do if v7[v12] then for v15=v12 * v12 ,v5,v12 do v7[v15]=false;end end end return v7;end break;end end end end local v4=sieve_of_eratosthenes(306 + 114 );for v8,v9 in pairs(v4) do if v9 then print("Prime found: "   .. v8 );end end end print("How to obfuscate best?");
+-- ⚠️ WARNING: integrity protected!
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.8) ~  Much Love, Ferib 
+
+]]--
